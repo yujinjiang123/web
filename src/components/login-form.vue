6d1fa4d75200dbf11cae9122ead06faf2eecb39e
964494258@qq.com <964494258@qq.com>
2018-11-27 08:28:16 +0800

    登录表单调整

--- src/components/login-form.vue
+++ src/components/login-form.vue
@@ -102,7 +102,7 @@ form {
 .login-form {
   height: 300px;
   padding-top: 60px;
-  margin-top: 80px;
+  margin-top: 40%;
   margin-bottom: 25px;
   background-color: rgba(255, 255, 255, 0.65);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <Row type="flex" justify="center" class="code-row-bg">
-      <Col :xs="{ span:20 }" :sm="{ span: 10}" :md="{ span: 8 }" :lg="{ span:6 }" :xl="{ span:6 }" >
+      <Col :xs="{ span:20 }" :sm="{ span: 10}" :md="{ span: 8 }" :lg="{ span:6 }" :xl="{ span:6 }">
         <login-form @listenToChileEvent="login"></login-form>
       </Col>
     </Row>
@@ -36,15 +36,6 @@ export default {
   background-image: url("./../assets/images/login-bg.jpg");
   background-size: cover;
   background-position: center;
-}
-
-.login-form {
-  height: 300px;
-  padding-top: 60px;
-  margin-top: 80px;
-  margin-bottom: 25px;
-  background-color: rgba(255, 255, 255, 0.65);
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
 
