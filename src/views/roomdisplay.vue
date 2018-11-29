ef25b4c9379eb8d898f3d45b0902d641cd2bcfa4
liuqiyun <1158783206@qq.com>
2018-11-29 22:15:54 +0800

    给我找错

--- src/views/roomdisplay.vue
+++ src/views/roomdisplay.vue
@@ -1,43 +1,35 @@
 <template>
   <div class="layout">
     <layout class="layout">
+
       <Head>
-        <!-- <search placeholder="搜索教室" slot="search"></search>        -->
+        <Button type="primary" @click="drawerVisible=true">筛选教室</Button>
         <dropdown slot="user">
           <useravatar slot="dropdown"></useravatar>
         </dropdown>
       </Head>
+
       <Layout>
         <!-- <Sider style="background-color:grey;" width='300'></Sider> -->
         <Content>
           <Row></Row>
-          <RoomCard style="height:663px"></RoomCard>
-          <div
-            style="position: fixed; bottom:300px;right:20px;cursor:pointer;background-color:#888;width:50px;height: 50px;"
-            @click="drawerVisible = true"
-          >
-            <Icon type="ios-arrow-back" size="40" style="margin:5px;"/>
+          <RoomCard style="height:663px" :rooms="searchRoomList"></RoomCard>
+          <div style="position: fixed; bottom:300px;right:20px;cursor:pointer;background-color:#888;width:50px;height: 50px;"
+            @click="drawerVisible = true">
+            <Icon type="ios-arrow-back" size="40" style="margin:5px;" />
           </div>
           <Drawer title="空闲教室查询" :closable="false" v-model="drawerVisible" width="300">
             <Form ref="searchForm" :model="searchForm" :rules="searchForm">
               <FormItem label="周次 :">
                 <br>
                 <Select v-model="searchForm.week" disabled>
-                  <Option
-                    v-for="item in weekList"
-                    :value="item.value"
-                    :key="item.value"
-                  >{{ item.label }}</Option>
+                  <Option v-for="item in weekList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                 </Select>
               </FormItem>
               <FormItem label="只能查看本周教室信息 :">
                 <br>
                 <Select v-model="searchForm.weekday" style="width:200px">
-                  <Option
-                    v-for="item in weekdayList"
-                    :value="item.value"
-                    :key="item.value"
-                  >{{ item.label }}</Option>
+                  <Option v-for="item in weekdayList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                 </Select>
               </FormItem>
               <FormItem label="空闲时段(第几节课) :">
@@ -63,56 +55,71 @@
 </template>
 
 <script>
-import RoomCard from "@/components/roomcard";
-import Head from "@/components/head";
-import Search from "@/components/search";
-import Dropdown from "@/components/dropdown";
-import UserAvatar from "@/components/userAvatar";
-export default {
-  components: {
-    RoomCard: RoomCard,
-    Head: Head,
-    search: Search,
-    dropdown: Dropdown,
-    useravatar: UserAvatar
-  },
-  data() {
-    return {
-      drawerVisible: false,
-      searchForm: {
-        week: "",
-        weekday: "",
-        lessonList: []
-      },
-      weekList: [],
-      weekdayList: []
-    };
-  },
-  methods: {
-    initialOption() {
+  import RoomCard from "@/components/roomcard";
+  import Head from "@/components/head";
+  import Search from "@/components/search";
+  import Dropdown from "@/components/dropdown";
+  import UserAvatar from "@/components/userAvatar";
+  import {
+    log
+  } from 'util';
+  export default {
+    components: {
+      RoomCard: RoomCard,
+      Head: Head,
+      search: Search,
+      dropdown: Dropdown,
+      useravatar: UserAvatar
+    },
+    data() {
+      return {
+        drawerVisible: false,
+        searchForm: {
+          weekday: "",
+          lessonList: []
+        },
+        weekList: [],
+        weekdayList: [],
+        searchRoomList: [],
+      };
+    },
+    methods: {
+      // initialOption() {
+      //   for (var i = 1; i < 21; i++) {
+      //     this.weekList.push({
+      //       value: i,
+      //       lable: "第" + i + "周"
+      //     });
+      //   }
+      // }
+      handleSearch() {
+        this.$api.getRoomList()
+          .then((data) => {
+            this.searchRoomList = data;
+          }, (res) => {
+            console.log('状态码:' + res.code);
+          })
+          .catch(err => {
+            console.log("数据获取失败");
+          })
+      }
+    },
+    mounted: function () {
       for (var i = 1; i < 21; i++) {
         this.weekList.push({
           value: i,
-          lable: "第" + i + "周"
+          label: "第" + i + "周"
+        });
+      }
+      for (var i = 1; i <= 7; i++) {
+        this.weekdayList.push({
+          value: i,
+          label: "星期" + i
         });
       }
     }
-  },
-  mounted: function() {
-    for (var i = 1; i < 21; i++) {
-      this.weekList.push({
-        value: i,
-        label: "第" + i + "周"
-      });
-    }
-    for (var i = 1; i <= 7; i++) {
-      this.weekdayList.push({
-        value: i,
-        label: "星期" + i
-      });
-    }
-  }
-};
+  };
+
 </script>
 <style scoped>
 </style>
