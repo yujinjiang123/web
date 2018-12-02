c2ae4fba0af47f766f23cad82147f849413b538b
964494258@qq.com <964494258@qq.com>
2018-12-02 22:04:03 +0800

    md5加密

--- src/components/login-form.vue
+++ src/components/login-form.vue
@@ -2,18 +2,23 @@
   <div class="login-form">
     <div class="head">
       <a class="head-item" @click="gotoLogin" style="color: #303133;">登录</a>
-      <a class="head-item" @click="gotoRegister" style="color: #303133;"> 注册 </a>
+      <a class="head-item" @click="gotoRegister" style="color: #303133;">注册</a>
     </div>
-    <Form v-if="view=='login'" class="form" ref="formInline" :model="formInline" :rules="ruleInline">
+    <Form
+      v-if="view=='login'"
+      class="form"
+      ref="formInline"
+      :model="formInline"
+      :rules="ruleInline"
+    >
       <FormItem prop="username">
         <Input size="large" type="text" v-model="formInline.username" placeholder="用户名" required>
-        </Input>
       </FormItem>
       <FormItem prop="password">
         <Input size="large" type="password" v-model="formInline.password" placeholder="密码">
-        </Input>
       </FormItem>
-      <a class="forget-password" @click="forgetPassword()">忘记密码</a><br/>
+      <a class="forget-password" @click="forgetPassword()">忘记密码</a>
+      <br>
       <FormItem>
         <Button class="submit" type="primary" @click="handleSubmit('formInline')">登录</Button>
       </FormItem>
@@ -21,24 +26,25 @@
     <Form v-if="view=='register'" class="form" ref="register" :model="register" :rules="ruleInline">
       <FormItem prop="registerUsername">
         <Input size="large" type="text" v-model="register.username" placeholder="用户名" required>
-      </Input>
       </FormItem>
       <FormItem prop="password1">
         <Input size="large" type="password" v-model="register.password1" placeholder="请输入密码">
-        </Input>
       </FormItem>
       <FormItem prop="password2">
         <Input size="large" type="password" v-model="register.password2" placeholder="确认密码">
-        </Input>
       </FormItem>
       <FormItem prop="email">
         <Input size="large" type="email" v-model="register.email" placeholder="邮箱地址">
-        </Input>
       </FormItem>
       <FormItem prop="code">
         <div style="display: flex">
-          <Input type="text" v-model="register.code" style="width:50%;" placeholder="请输入验证码"></Input>
-          <Button @click="sendEmail" class="send-code"  style="width:50%;" :disabled="canClick">{{codeInfo}}</Button>
+          <Input type="text" v-model="register.code" style="width:50%;" placeholder="请输入验证码">
+          <Button
+            @click="sendEmail"
+            class="send-code"
+            style="width:50%;"
+            :disabled="canClick"
+          >{{codeInfo}}</Button>
         </div>
       </FormItem>
       <FormItem>
@@ -48,24 +54,25 @@
   </div>
 </template>
 <script>
+// import md5 from "../utils/md5.js";
 export default {
   data() {
     return {
       totalTime: 60,
       canClick: false, //添加canClick
-      code:'',
-      codeInfo:'获取验证码',
-      view: 'login',
+      code: "",
+      codeInfo: "获取验证码",
+      view: "login",
       formInline: {
         username: "",
         password: ""
       },
       register: {
-        username: '',
-        email: '',
-        password1: '',
-        password2: '',
-        code: ''
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+        code: ""
       },
       ruleInline: {
         username: [
@@ -119,7 +126,7 @@ export default {
             min: 6,
             message: "密码不能少于六位",
             trigger: "blur"
-          },
+          }
         ],
         email: [
           {
@@ -135,21 +142,21 @@ export default {
             trigger: "blur"
           }
         ]
-      },
-    }
+      }
+    };
   },
   methods: {
-    gotoLogin(){
-      if(this.$refs['register']!=undefined){
-        this.$refs['register'].resetFields();
+    gotoLogin() {
+      if (this.$refs["register"] != undefined) {
+        this.$refs["register"].resetFields();
       }
-      this.view="login";
+      this.view = "login";
     },
-    gotoRegister(){
-      if(this.$refs['formInline']!=undefined){
-        this.$refs['formInline'].resetFields();
+    gotoRegister() {
+      if (this.$refs["formInline"] != undefined) {
+        this.$refs["formInline"].resetFields();
       }
-      this.view="register";
+      this.view = "register";
     },
     handleSubmit(name) {
       console.log(name);
@@ -168,29 +175,28 @@ export default {
         name: "findPassword"
       });
     },
-    sendEmail(){
-      if(this.register.email===""){
-        this.$Message.warning('请填写邮箱');
-      }else{
+    sendEmail() {
+      if (this.register.email === "") {
+        this.$Message.warning("请填写邮箱");
+      } else {
         this.countDown();
       }
     },
-    countDown(){
-      if (this.canClick) return;  //改动的是这两行代码
+    countDown() {
+      if (this.canClick) return; //改动的是这两行代码
       this.canClick = true;
-      this.codeInfo = this.totalTime + 's后重新发送';
+      this.codeInfo = this.totalTime + "s后重新发送";
       let clock = window.setInterval(() => {
         this.totalTime--;
-        this.codeInfo = this.totalTime + 's后重新发送';
+        this.codeInfo = this.totalTime + "s后重新发送";
         if (this.totalTime < 0) {
           window.clearInterval(clock);
-          this.codeInfo = '重新发送验证码';
+          this.codeInfo = "重新发送验证码";
           this.totalTime = 60;
-          this.canClick = false  //这里重新开启
+          this.canClick = false; //这里重新开启
         }
-      },1000)
+      }, 1000);
     }
-
   }
 };
 </script>
@@ -202,31 +208,31 @@ form {
   margin-top: 30px;
 }
 
-.head{
-  width:100%;
+.head {
+  width: 100%;
   display: flex;
   font-size: 20px;
   margin-bottom: 1px;
-  color:#303133;
-  border-bottom: 1px solid #DCDFE6;
+  color: #303133;
+  border-bottom: 1px solid #dcdfe6;
 }
 
 .head-item {
-  width:50%;
+  width: 50%;
   height: 100%;
   text-align: center;
-  padding:8px 0;
-  border-right: 1px solid #DCDFE6;
-  border-bottom: 2px solid rgba(255,255,255,0);
+  padding: 8px 0;
+  border-right: 1px solid #dcdfe6;
+  border-bottom: 2px solid rgba(255, 255, 255, 0);
   cursor: hand;
 }
 
-.head-item:hover{
-  border-bottom: 2px solid #DCDFE6;
+.head-item:hover {
+  border-bottom: 2px solid #dcdfe6;
 }
 
-.form{
-  margin-top:20px;
+.form {
+  margin-top: 20px;
   padding-bottom: 20px;
 }
 
@@ -248,5 +254,4 @@ form {
   background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
-
 </style>
